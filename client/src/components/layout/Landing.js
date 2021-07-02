57f80bfd6bbb1361ff0ae41df6472be1531f1a41
Aniket Arora <aniketarora568@gmail.com>
2021-07-02 23:18:47 +0530

    restored

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -29,7 +29,7 @@ const Landing = ({ isAuthenticated }) => {
         
         </div>
         <footer className="landing-footer"> 
-     <p>copyright © 2020 </p> 
+     <p>copyright © 2020 Aniket Arora</p> 
       </footer>
         
       </div>
